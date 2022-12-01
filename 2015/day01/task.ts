9ed759c093afa5550330584ff49beac028f9e48e
Egill Anton Hlöðversson <egillanton@users.noreply.github.com>
2022-12-01 22:03:59 +0400

    Add 2015.01.p2

--- 2015/day01/task.ts
+++ 2015/day01/task.ts
@@ -16,3 +16,21 @@ for(var i = 0; i < input.length; i++) {
 }
 
 console.log(`Santa is on floor ${currentFloor} after following the instructions.`);
+
+// Part Two
+currentFloor = 0;
+var santaFirstTimeEnterBasementIndex = -1;
+for(var i = 0; i < input.length; i++) {
+	var char = input[i];
+	if(char == "(") {
+		currentFloor++;
+	} else if(char == ")") {
+		currentFloor--;
+	}
+	if(currentFloor == -1) {
+		santaFirstTimeEnterBasementIndex = i + 1;
+		break;
+	}
+}
+
+console.log(`Santa enters the basement at position ${santaFirstTimeEnterBasementIndex}.`);
