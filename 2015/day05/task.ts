679d7ebe7e6b9305587e52a8ffe5bb722790dd08
Egill Anton Hlöðversson <egillanton@users.noreply.github.com>
2022-12-01 23:38:11 +0400

    Add 2015.05.p1

---
+++ 2015/day05/task.ts
@@ -0,0 +1,46 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+
+// Part One
+var lines = input.split("\n").map(line => line.trim());
+
+var vowels = ["a", "e", "i", "o", "u"];
+var naughtyStrings = ["ab", "cd", "pq", "xy"];
+
+var niceStrings = 0;
+
+console.log("Total number of strings: " + lines.length);
+for (let i = 0; i < lines.length; i++) {
+  var line = lines[i];
+  if (line.length === 0) {
+    continue;
+  }
+  let vowelCount = 0;
+  let hasDoubleLetter = false;
+  let hasNaughtyString = false;
+
+  for (let j = 0; j < line.length; j++) {
+    var letter = line[j];
+    // check for vowels
+    if (vowels.indexOf(letter) != -1) {
+      vowelCount++;
+    }
+    if (j > 0 && line[j - 1] == letter) {
+      hasDoubleLetter = true;
+    }
+    if (j > 1 && naughtyStrings.indexOf(line.substring(j - 1, j + 1)) != -1) {
+      hasNaughtyString = true;
+    }
+  }
+
+  if (vowelCount >= 3 && hasDoubleLetter && !hasNaughtyString) {
+    niceStrings++;
+  }
+}
+
+console.log(`Number of nice strings: ${niceStrings}`); // Number of nice strings: 239
+
+// Is there a of-by-one bug in the puzzle?
+// My calculation is 239, but the puzzle says 238.
