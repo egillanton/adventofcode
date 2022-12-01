76396fac5f6447e480d34b5d76c59818f1735b87
Egill Anton Hlöðversson <egillanton@users.noreply.github.com>
2022-12-01 22:25:37 +0400

    Add 2015.01.p2

--- 2015/day02/task.ts
+++ 2015/day02/task.ts
@@ -19,3 +19,20 @@ for (var i = 0; i < lines.length; i++) {
 }
 
 console.log(`totalPaper: ${totalPaper} square feet of wrapping paper`);
+
+// Part Two
+var totalRibbon = 0;
+for (var i = 0; i < lines.length; i++) {
+  var line = lines[i];
+  var [x, y, z] = line.split("x").map((x) => parseInt(x));
+  var smallestSide = Math.min(x * y, y * z, z * x);
+  var volume = x * y * z;
+  var smallestPerimeter = Math.min(2 * (x + y), 2 * (y + z), 2 * (z + x));
+
+  var lengthOfRibbonToWrapThePresent = smallestPerimeter;
+  var lengthOfRibbonForBow = volume;
+  var ribbon = lengthOfRibbonToWrapThePresent + lengthOfRibbonForBow;
+  totalRibbon += ribbon;
+}
+
+console.log(`Total ribbon: ${totalRibbon} feet.`);
