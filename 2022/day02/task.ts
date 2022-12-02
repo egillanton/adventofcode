cb5a64f98430577aac802de9554b4b4327b5f56c
Egill Anton Hlöðversson <egillanton@users.noreply.github.com>
2022-12-02 10:30:07 +0400

    Add 2022.02.p2

--- 2022/day02/task.ts
+++ 2022/day02/task.ts
@@ -5,10 +5,10 @@ const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
 
 // Part One
 // Rock paper scissors map
-var actionToActionMap = new Map();
-actionToActionMap.set("Rock", "Scissors"); // Rock beats scissors
-actionToActionMap.set("Paper", "Rock"); // Paper beats rock
-actionToActionMap.set("Scissors", "Paper"); // Scissors beats paper
+var winActionMap = new Map();
+winActionMap.set("Rock", "Scissors"); // Rock beats scissors
+winActionMap.set("Paper", "Rock"); // Paper beats rock
+winActionMap.set("Scissors", "Paper"); // Scissors beats paper
 
 var player1ActionMap = new Map();
 player1ActionMap.set("A", "Rock"); // A = Rock
@@ -38,7 +38,7 @@ for (var i = 0; i < lines.length; i++) {
   // console.log(player1ActionName);
   var player2ActionName = player2ActionMap.get(player2Action);
   // console.log(player2ActionName);
-  var player1ActionWins = actionToActionMap.get(player1ActionName);
+  var player1ActionWins = winActionMap.get(player1ActionName);
   // console.log(player1ActionWins);
 
   if (player1ActionWins == player2ActionName) {
@@ -50,6 +50,40 @@ for (var i = 0; i < lines.length; i++) {
   } else {
     // console.log("Player 2 wins");
     totalScore += 6 + scoreMap.get(player2ActionName);
+  }
+}
+
+console.log(`Total score: ${totalScore}`);
+
+// Part two
+totalScore = 0;
+var howTheRoundShouldEndMap = new Map();
+howTheRoundShouldEndMap.set("X", 0); // Play one needs to win
+howTheRoundShouldEndMap.set("Y", 3); // Players need to draw
+howTheRoundShouldEndMap.set("Z", 6); // Player two needs to win 
+
+var losingActionMap = new Map();
+losingActionMap.set("Rock", "Paper"); // Rock looses against scissors
+losingActionMap.set("Paper", "Scissors"); // Paper looses against rock
+losingActionMap.set("Scissors", "Rock"); // Scissors looses against paper
+
+for (var i = 0; i < lines.length; i++) {
+  var line = lines[i];
+  var [player1Action, howTheRoundShouldEnd] = line.split(" ").map((x) => x.trim());
+
+  var player1ActionName = player1ActionMap.get(player1Action);
+  if(howTheRoundShouldEnd == "X"){
+    // Player 1 needs to win
+    var losingActionName = winActionMap.get(player1ActionName);
+    totalScore += scoreMap.get(losingActionName);
+  } else if(howTheRoundShouldEnd == "Y"){
+    // Players need to draw
+    totalScore += 3 + scoreMap.get(player1ActionName);
+  }
+  else {
+    // Player 2 needs to win
+    var winningActionName = losingActionMap.get(player1ActionName);
+    totalScore += 6 + scoreMap.get(winningActionName);
   }
 }
 
