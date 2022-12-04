4bdbfc354766801164c0e5a0beee665bb6c27340
Egill Anton Hlöðversson <egillanton@users.noreply.github.com>
2022-12-04 12:04:42 +0400

    2022.04.p2

--- 2022/day04/task.ts
+++ 2022/day04/task.ts
@@ -33,4 +33,34 @@ for (var i = 0; i < lines.length; i++) {
   }
 }
 
+// Answer: 305
 console.log(oneRangeFullyContainTheOther);
+
+var overlapCount = 0;
+for (var i = 0; i < lines.length; i++) {
+  var line = lines[i];
+  var ranges = line.split(",");
+  var range1 = ranges[0].split("-");
+  var range2 = ranges[1].split("-");
+  var range1Start = parseInt(range1[0]);
+  var range1End = parseInt(range1[1]);
+  var range2Start = parseInt(range2[0]);
+  var range2End = parseInt(range2[1]);
+
+  console.log(`Range 1: ${range1Start} - ${range1End}`);
+  console.log(`Range 2: ${range2Start} - ${range2End}`);
+
+  // Minor change to the part one
+  if (
+    (range1Start <= range2Start && range2Start <= range1End) ||
+    (range2Start <= range1Start && range1Start <= range2End)
+  ) {
+    console.log("One range fully contains the other");
+    console.log(`Range 1: ${range1Start} - ${range1End}`);
+    console.log(`Range 2: ${range2Start} - ${range2End}`);
+    overlapCount++;
+  }
+}
+
+// Answer: 811
+console.log(overlapCount);
