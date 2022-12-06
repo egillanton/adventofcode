4b885a211da266a8432aba718a6cef12ac782b02
Egill Anton Hlöðversson <egillanton@users.noreply.github.com>
2022-12-06 11:47:09 +0400

    2022.06.p2

--- 2022/day06/task.ts
+++ 2022/day06/task.ts
@@ -4,25 +4,20 @@ import * as path from "path";
 const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
 
 // Day 6: Tuning Trouble
-
-var lines = input.split("\n");
-
+var distinctCharacters = 14;
 var sum = 0;
-for (var i = 0; i < lines.length; i++) {
-  var line = lines[i];
-  var chars = line.split("");
-  for (var j = 3; j < chars.length; j++) {
-    var set = new Set();
-    set.add(chars[j-3]);
-    set.add(chars[j-2]);
-    set.add(chars[j-1]);
-    set.add(chars[j]);
-    console.log(set)
-    console.log(set.size)
-    if(set.size == 4) {
-      sum += j;
-      break;
-    }
+
+var chars = input.trim().split("");
+for (var i = (distinctCharacters-1); i < chars.length; i++) {
+  var set = new Set();
+
+  for (var j = (distinctCharacters-1); j >= 0; j--) {
+    set.add(chars[i-j]);
+  }
+
+  if(set.size == distinctCharacters) {
+    sum += i;
+    break;
   }
 }
 
