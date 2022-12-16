2c70f5447742b4de43a8dfc321b91148c30473b7
Egill Anton Hlöðversson <egillanton@users.noreply.github.com>
2022-12-16 22:42:33 +0400

    2022.09

---
+++ 2022/day09/task.ts
@@ -0,0 +1,11 @@
+// Day 9: Rope Bridge
+import * as fs from "fs";
+import * as path from "path";
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+
+// Part 1
+var positionsVisited = 0;
+
+console.log("How many positions does the tail of the rope visit at least once?");
+console.log("Answer:", positionsVisited);
