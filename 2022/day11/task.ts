ac5d5baaeebcbdc926f3b910b2df307ba4408ba9
Egill Anton Hlöðversson <egillanton@users.noreply.github.com>
2022-12-17 22:36:01 +0400

    2022.10

---
+++ 2022/day11/task.ts
@@ -0,0 +1,12 @@
+// Day 10: Cathode-Ray Tube
+import * as fs from "fs";
+import * as path from "path";
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+
+// Part 1: What is the level of monkey business after 20 rounds of stuff-slinging simian shenanigans?
+
+var monkeyBusinessLevel = 0;
+
+console.log("What is the level of monkey business after 20 rounds of stuff-slinging simian shenanigans?");
+console.log("Answer:");
