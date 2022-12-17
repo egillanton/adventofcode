c6bb8492e022568a4d1c65f6380af2c72d7ca49b
Egill Anton Hlöðversson <egillanton@users.noreply.github.com>
2022-12-17 14:10:56 +0400

    2022.10.p2

--- 2022/day10/task.ts
+++ 2022/day10/task.ts
@@ -31,54 +31,120 @@ function parseInput(input: string) {
   return instructions;
 }
 
+var INSTRUCTIONS = parseInput(input);
+
+function partOne() {
+  var x = 1;
+  var temp: number = null;
+  var intervals = [20, 60, 100, 140, 180, 220];
+  var intervalOutputs: number[] = [];
+  var cycleCount = 1;
+
+  var i = 0;
+  var cpuBusy = false;
+  while (i < INSTRUCTIONS.length || cpuBusy) {
+    console.log(`START:${cycleCount}: ${x}`);
+    if (intervals.includes(cycleCount)) {
+      var intervalSignalStrength = cycleCount * x;
+      intervalOutputs.push(intervalSignalStrength);
+    }
+
+    if (cpuBusy) {
+      console.log(`DURING:${cycleCount}: ${x} -> ${x + temp}`);
+      cycleCount += 1;
+      x += temp;
+      temp = null;
+      cpuBusy = false;
+    } else {
+      var instruction = INSTRUCTIONS[i];
+      console.log(
+        `DURING:${cycleCount}: Executing instruction: ${instruction.action}${
+          instruction.delta ? " " + instruction.delta : ""
+        }`
+      );
+      if (instruction.action == "addx") {
+        temp = INSTRUCTIONS[i].delta;
+        cpuBusy = true;
+      }
+      cycleCount += 1;
+      i++;
+    }
+    console.log(`END:${cycleCount - 1}: ${x}`);
+    console.log();
+  }
+
+  // set signalStrengthSum as the sum of intervalOutputs
+  var signalStrengthSum = intervalOutputs.reduce((a, b) => a + b, 0);
+
+  for (let i = 0; i < intervalOutputs.length; i++) {
+    console.log(
+      `Signal strength at interval: ${intervals[i]} = ${intervalOutputs[i]}`
+    );
+  }
+  console.log("What is the sum of these six signal strengths?");
+  console.log("Answer:", signalStrengthSum); // Answer: 14720
+}
+
+// partOne();
+
+// Part 2: What eight capital letters appear on your CRT?
+
+const CRT_SIZE = {
+  WIDTH: 40, // pixels
+  HEIGHT: 6, // pixels
+};
+
+var crtGrid: string[][] = new Array(CRT_SIZE.HEIGHT)
+  .fill("")
+  .map(() => new Array(CRT_SIZE.WIDTH).fill("."));
+
+function printCrtGrid(crtGrid: string[][]) {
+  for (var i = 0; i < crtGrid.length; i++) {
+    console.log(crtGrid[i].join(""));
+  }
+}
+
 var x = 1;
 var temp: number = null;
-var intervals = [20, 60, 100, 140, 180, 220];
-var intervalOutputs: number[] = [];
-var cycleCount = 1;
-var instructions = parseInput(input);
+var cycleCount = 0;
 
 var i = 0;
 var cpuBusy = false;
-while (i < instructions.length || cpuBusy) {
-  console.log(`START:${cycleCount}: ${x}`);
-  if (intervals.includes(cycleCount)) {
-    var intervalSignalStrength = cycleCount * x;
-    intervalOutputs.push(intervalSignalStrength);
-  }
+while (i < INSTRUCTIONS.length || cpuBusy) {
+	
+	if(x  == cycleCount%CRT_SIZE.WIDTH || x == cycleCount%CRT_SIZE.WIDTH +1 || x == cycleCount%CRT_SIZE.WIDTH -1)
+		crtGrid[Math.floor(cycleCount/CRT_SIZE.WIDTH)][cycleCount%CRT_SIZE.WIDTH] = "#";
 
   if (cpuBusy) {
-    console.log(`DURING:${cycleCount}: ${x} -> ${x + temp}`);
+	console.log(`Start cycle\t${cycleCount}: Register X is now ${x}`);
+	console.log(
+      `End of cycle\t${cycleCount}: finish executing addx ${temp} (Register X is now ${
+        x + temp
+      })`
+    );
+	console.log();
     cycleCount += 1;
     x += temp;
     temp = null;
     cpuBusy = false;
   } else {
-    var instruction = instructions[i];
-    console.log(
-      `DURING:${cycleCount}: Executing instruction: ${instruction.action}${
-        instruction.delta ? " " + instruction.delta : ""
-      }`
-    );
-    if (instruction.action == "addx") {
-      temp = instructions[i].delta;
+    if (INSTRUCTIONS[i].action == "addx") {
+      console.log(
+        `Start cycle\t${cycleCount}: begin executing ${INSTRUCTIONS[i].action} ${INSTRUCTIONS[i].delta}`
+      );
+      temp = INSTRUCTIONS[i].delta;
       cpuBusy = true;
-    }
+    } else{
+		console.log(`Start cycle\t${cycleCount}: begin executing ${INSTRUCTIONS[i].action}`);
+	}
     cycleCount += 1;
     i++;
+	console.log(`End of cycle\t${cycleCount - 1}: ${x}`);
+	console.log();
   }
-  console.log(`END:${cycleCount - 1}: ${x}`);
-  console.log();
 }
 
-// set signalStrengthSum as the sum of intervalOutputs
-var signalStrengthSum = intervalOutputs.reduce((a, b) => a + b, 0);
+printCrtGrid(crtGrid);
 
-for (let i = 0; i < intervalOutputs.length; i++) {
-  console.log(
-    `Signal strength at interval: ${intervals[i]} = ${intervalOutputs[i]}`
-  );
-}
-
-console.log("What is the sum of these six signal strengths?");
-console.log("Answer:", signalStrengthSum); // Answer: 14720
+console.log("What eight capital letters appear on your CRT?");
+console.log("Answer: FZBPBFZF"); // Answer: 14720
